0cea64f148bfa2a2add0278dcd0e7a41d24a6a62
Симкин Сергей <ssimkin@c-blues.com>
2018-05-05 03:03:31 +0300

    add user entity

---
+++ SimpleBugTracker/Controllers/MainController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SimpleBugTracker.Models;
+
+namespace SimpleBugTracker.Controllers
+{
+    public class MainController
+    {
+        [Route("api/[controller]")]
+        public class ValuesController : Controller
+        {
+            static readonly List<User> _users;
+            static ValuesController()
+            {
+                _users = new List<User>
+            {
+                new User { FirstName="S", SecondName="S", Login = "Krolik", Password="krolik"},
+                new User {  FirstName="S", SecondName="S", Login = "Krolik1", Password="krolik" },
+            };
+            }
+            [HttpGet]
+            public IEnumerable<User> Get()
+            {
+                return _users;
+            }
+
+            [HttpPost]
+            public IActionResult LogIn([FromBody]User phone)
+            {
+                return Ok(phone);
+            }
+
+            [HttpDelete("{id}")]
+            public IActionResult Delete(string id)
+            {
+                return Ok(id);
+            }
+        }
+    }
+}
---
+++ SimpleBugTracker/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBugTracker.Models
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Password { get; set; }
+    }
+}
