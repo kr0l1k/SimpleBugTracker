80cf4057f12a1234b8e2d35e0b4706e2b56d544b
Симкин Сергей <ssimkin@c-blues.com>
2018-05-17 22:25:24 +0300

    nav bar css

--- BugTracker/ClientApp/components/NavMenu.tsx
+++ BugTracker/ClientApp/components/NavMenu.tsx
@@ -36,21 +36,19 @@ export class NavMenu extends React.Component<{}, {}> {
         //        </div>
         //    </div>
         //</div>;
-        return <div className='nav'>
-            <div className='main-nav'>
-                <ul className='nav navbar-nav'>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
-                    <li className="dropdown">
-                        <a href="javascript:void(0)" className="dropbtn">Dropdown</a>
-                        <div className="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+        return <div className='main-nav'>
+            <ul className='nav navbar-nav'>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li className="dropdown">
+                    <a href="javascript:void(0)" className="dropbtn">Dropdown</a>
+                    <div className="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </li>
+            </ul>
         </div>;
 
     }
